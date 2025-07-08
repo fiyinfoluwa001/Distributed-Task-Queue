7c9a978a8a70714e213c8b7c26089b4401c14d08
boluwatife010 <boluwatifehonour@gmail.com>
2025-07-08 19:11:33 +0100

    working on the server for worker.

--- src/producer/server.ts
+++ src/producer/server.ts
@@ -1,5 +1,32 @@
 import express from 'express'
 import {Queue} from 'bullmq'
+import {v4 as uuidv4} from 'uuid'
 import {createBullBoard} from '@bull-board/api'
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { ExpressAdapter } from '@bull-board/express'
+const connection = {host: 'localhost', port: 6379}
+const taskQueue = new Queue('tasks', {connection})
+const app = express()
+app.use(express.json())
+// Board for UI monitoring
+const serverAdapter = new ExpressAdapter()
+createBullBoard({
+    queues: [new BullMQAdapter(taskQueue)],
+    serverAdapter
+})
+serverAdapter.setBasePath('/admin');
+app.use('/admin', serverAdapter.getRouter())
+
+// Task submission endpoint
+app.post('/tasks', async (req, res) => {
+    const {type, data, priority} = req.body;
+    const job = await taskQueue.add(type, data, {
+        priority: priority || 'normal',
+        jobId: uuidv4()
+    })
+    res.send({id: job.id})
+})
+
+app.listen(4000, () => {
+    console.log('Producer running on port 4000.')
+})
