86ed8ff269ff2d8fa7a89915a3c7d8ec0fb93ab6
fiyinfoluwa001 <boluwatifehonour@gmail.com>
2025-08-22 14:43:21 +0100

    added test file.

--- api/src/tasks/tasks.controller.spec.ts
+++ api/src/tasks/tasks.controller.spec.ts
@@ -10,7 +10,8 @@ describe('TasksController', () => {
       controllers: [TasksController],
       providers: [
         TasksService,
-        { provide: 'PrismaService', useValue: {} },
+        { provide: 'PrismaService', useValue: {task: {create: jest.fn(), findMany: jest.fn(),  findUnique: jest.fn(), update: jest.fn()}} },
+        {provide: 'AuthService', useValue: {validateToken: jest.fn(() => ({role: 'admin'}))}}
       ],
     }).compile();
     controller = module.get<TasksController>(TasksController);
@@ -19,4 +20,8 @@ describe('TasksController', () => {
   it('should create a task', async () => {
     expect(controller.create({ payload: { test: true } })).toBeDefined();
   });
+  it ('should sync tasks', async () => {
+    const result = await controller.syncTasks({tasks: [{payload: {test: true}, priority: 1}]})
+    expect(result).toBeDefined()
+  })
 });
